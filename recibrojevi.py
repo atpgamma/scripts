eab193314d15b5ce3e396c06eec7b989542d4eef
stevn <fraktalnavreca@tutanota.com>
2016-12-02 23:19:41 +0100

    Dodato da ispisuje u fajl

--- recibrojevi.py
+++ recibrojevi.py
@@ -2,9 +2,11 @@ brojevi = ['jedan','dva','tri','cetiri','pet','sest','sedam','osam','devet','des
 
 
 f = open('reci.txt','r')
+out = open('out.txt','w')
 for line in f.readlines():
 	for broj in brojevi:
 		if broj in line:
 			print(line)
+			out.write(line)
 
 f.close()
