11addbb8fc5c39baa13693d1131cdfca4534d5be
stevn <fraktalnavreca@tutanota.com>
2016-12-15 00:19:28 +0100

    Done something

--- reci/recibrojevi.py
+++ reci/recibrojevi.py
@@ -1,12 +1,23 @@
-brojevi = ['jedan','dva','tri','cetiri','pet','sest','sedam','osam','devet','deset']
+import sys
+
+brojevi = ['jedan','dva','tri','cetiri','pet','sest','sedam','osam','devet','deset','nula','sto']
 
 
-f = open('reci.txt','r')
-out = open('out.txt','w')
-for line in f.readlines():
-	for broj in brojevi:
-		if broj in line:
+if(len(sys.argv) > 1):
+	rec = sys.argv[1]
+	f = open('reci.txt','r')
+	out = open('out.txt','w')
+	for line in f.readlines():
+		if rec in line:
 			print(line)
-			out.write(line)
+else:
+	f = open('reci.txt','r')
+	out = open('out.txt','w')
+	for line in f.readlines():
+		for broj in brojevi:
+			if broj in line:
+				print(line)
+				out.write(line)
 
-f.close()
+	f.close()
+	out.close()
